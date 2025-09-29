12df3d58ec95a2f2f82fbebbf62164fb88e99834
nickpasta04 <nskkim@ucdavis.edu>
2025-09-29 16:30:41 -0700

    Update DATAprofrel.ts

--- src/app/officers/DATAprofrel.ts
+++ src/app/officers/DATAprofrel.ts
@@ -90,7 +90,7 @@ export const profrel = [
     cohort: 'Spring 2024 Cohort',
     position: 'Professional Relations',
     linkedin: 'https://www.linkedin.com/in/hishaanrajwani/',
-    image: '/officers/Rajwani_Hishaan.jpg',
+    image: '/officers/Rajwani_Hishaan.jpeg',
   },
   // {
   //   name: 'Sukhman Kahlon',
